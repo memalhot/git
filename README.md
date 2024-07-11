130bba175c3e40bc495a9d3e923adfca316defe5
meera malhotra <memalhot@redhat.com>
2024-07-11 09:41:45 -0400

    git helper script arg updates

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# git
+# Git Helper Script Guide
+
+To get started, first cd into the folder *git*, and then \
+run the helper git setup script with your name and the email associated with your github account
+
+***Note: make sure you type your name as one word!*** \
+Like this:
+```./githelp.sh <your name> <your github email>```
+
+So when running, it would look like: \
+```./githelp.sh jenny jennyjones@gmail.com```
---
+++ githelp.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#for readability and instructions
+RED='\033[1;31m' #Bold red
+NC='\033[0m' # No Color
+BLUE='\033[0;36m' # Blue
+BLACK='\033[1;30m' # Bold Black
+
+#Name and github email
+EXPECTED_ARGS=2
+
+echo "you must have a github account to continue with this process"
+echo -e "if you don't, press ctrl-c, create an account here:${BLUE} https://docs.github.com/en/get-started/start-your-journey/creating-an-account-on-github${NC}"
+echo "and rerun this script once you have created one"
+
+#check if student provides both name and email
+if [ $# -ne $EXPECTED_ARGS ]; then
+    echo "Please provide your name as one word, and the email associated with your github"
+    echo "like this: ./githelp.sh jenna jenloveslinux@gmail.com"
+    echo "rerun with the correct format!"
+    exit 1
+fi
+
+#configure git w name and email
+git config --global user.name $1
+git config --global user.email $2
+
+echo " "
+echo -e "${BLACK}this next step will create your key to connect with github, when prompted"
+echo -e "${RED}press enter ${BLACK} for each prompt, or choose where you want to save your key${NC}"
+echo " "
+
+#generate key to link w github
+ssh-keygen
+
+echo " "
+echo -e "${RED}COPY THE WHOLE STRING BELOW INTO GITHUB HERE: ${BLUE} https://github.com/settings/keys ${NC}"
+cat ~/.ssh/id_rsa.pub
+echo " "
+
+#don't have all the instructions print out at once
+sleep 3s
+echo "once you've copied the string above into your ssh keys on github"
+echo "your github is set up in the lab environment!"
+echo "for more information or trouble shooting help, check out:"
+echo -e "${BLUE}https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent${NC}"
+
